2eac5f547573c6ea6f799df0df8d4d711a9453f1
Pooja Tewari <poojatewrai410@gmail.com>
2018-06-01 14:49:59 -0400

    New commits

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -5,6 +5,7 @@ $('#business_type_select').on("change", function(e) {
 
   $("#business_type_select").css("border", "solid 1px #eaeaea");
   var val = $("#business_type_select").val();
+   
   if(val == "local") {
     $(".local_field").css('display', 'block');
     $(".online_field").css('display', 'none');
@@ -25,12 +26,13 @@ $('#business_type_select').on("change", function(e) {
     $(".local_field").attr('required', 'required');
     $(".online_field").attr('required', 'required');
   }
-  else if(val = "none") {
+  else if(val == "none") {
     $(".local_field").css('display', 'none');
     $(".online_field").css('display', 'none');
     $(".local_field").removeAttr('required');
     $(".online_field").removeAttr('required');
   }
+ 
 });
 
 // $(document).ready( function() {
